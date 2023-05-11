e4ccd63c91a06ef9c2a84c712f6833b44e32919c
smajaas <smajaas@gmail.com>
2023-05-11 16:26:03 +0530

    target link changed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <a href='https://vitejs.dev' target='_blank'>
+        <a href='https://vitejs.dev' target='_blank' rel=';noreferer'>
           <img src={viteLogo} className='logo' alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
